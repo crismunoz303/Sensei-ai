35540e8e92be831a9940754da8f6eeb55331fbc5
crismunoz303 <crxis.1600@gmail.com>
2026-09-20 02:51:09 -0700

    Handle background model download events

--- Sensei/SenseiApp.swift
+++ Sensei/SenseiApp.swift
@@ -1,7 +1,25 @@
 import SwiftUI
+import UIKit
+
+final class SenseiAppDelegate: NSObject, UIApplicationDelegate {
+    func application(
+        _ application: UIApplication,
+        handleEventsForBackgroundURLSession identifier: String,
+        completionHandler: @escaping () -> Void
+    ) {
+        guard identifier == BackgroundModelDownloadManager.sessionIdentifier else {
+            completionHandler()
+            return
+        }
+
+        BackgroundModelDownloadManager.shared
+            .setBackgroundEventsCompletionHandler(completionHandler)
+    }
+}
 
 @main
 struct SenseiApp: App {
+    @UIApplicationDelegateAdaptor(SenseiAppDelegate.self) private var appDelegate
     @StateObject private var chat = ChatViewModel()
 
     var body: some Scene {
