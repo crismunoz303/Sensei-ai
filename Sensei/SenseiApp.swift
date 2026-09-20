e61c87ded042e0359fca534957972fd3c799d358
crismunoz303 <crxis.1600@gmail.com>
2026-09-20 09:05:22 -0700

    Add background download notifications and handoff

--- Sensei/SenseiApp.swift
+++ Sensei/SenseiApp.swift
@@ -1,7 +1,25 @@
 import SwiftUI
 import UIKit
+import UserNotifications
 
-final class SenseiAppDelegate: NSObject, UIApplicationDelegate {
+final class SenseiAppDelegate: NSObject, UIApplicationDelegate, UNUserNotificationCenterDelegate {
+    func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil
+    ) -> Bool {
+        UNUserNotificationCenter.current().delegate = self
+        _ = BackgroundModelDownloadManager.shared
+        return true
+    }
+
+    func userNotificationCenter(
+        _ center: UNUserNotificationCenter,
+        willPresent notification: UNNotification,
+        withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void
+    ) {
+        completionHandler([.banner, .sound])
+    }
+
     func application(
         _ application: UIApplication,
         handleEventsForBackgroundURLSession identifier: String,
@@ -20,6 +38,7 @@ final class SenseiAppDelegate: NSObject, UIApplicationDelegate {
 @main
 struct SenseiApp: App {
     @UIApplicationDelegateAdaptor(SenseiAppDelegate.self) private var appDelegate
+    @Environment(\.scenePhase) private var scenePhase
     @StateObject private var chat = ChatViewModel()
 
     var body: some Scene {
@@ -27,6 +46,16 @@ struct SenseiApp: App {
             ContentView()
                 .environmentObject(chat)
                 .preferredColorScheme(.dark)
+                .onChange(of: scenePhase) { _, newPhase in
+                    switch newPhase {
+                    case .background:
+                        BackgroundModelDownloadManager.shared.beginBackgroundHandoff()
+                    case .active:
+                        BackgroundModelDownloadManager.shared.endBackgroundHandoff()
+                    default:
+                        break
+                    }
+                }
         }
     }
 }
